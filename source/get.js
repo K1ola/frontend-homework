61702fbd53aad06328624d33da42f80fcd9acb77
kate <kirilek@bk.ru>
2019-02-10 02:50:31 +0300

    changed property check

--- source/get.js
+++ source/get.js
@@ -11,7 +11,7 @@ const get = (obj, string) => {
     const arrayKeys = string.slice(1).split('.');
 
     for (let value of arrayKeys) {
-        if (obj.hasOwnProperty(value)) obj = obj[value];
+        if (({}).hasOwnProperty.call(obj, value)) obj = obj[value];
         else {
             return;
         }
